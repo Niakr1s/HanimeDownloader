13586528f67b3582480e93951fc54febe2b587ac
Niakr1s <pavel2188@gmail.com>
2025-09-15 06:44:06 +0300

    refactored logger module

--- helpers/log/logger.py
+++ helpers/log/logger.py
@@ -1,19 +1,19 @@
 import logging
 
-# create logger with 'spam_application'
 logger = logging.getLogger("hanime_downloader")
 logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
+logger.propagate = False
+
 fh = logging.FileHandler("hanime_downloader.log")
 fh.setLevel(logging.DEBUG)
-# create console handler with a higher log level
+
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
-# create formatter and add it to the handlers
-formatter = logging.Formatter("%(asctime)s|%(levelname)s|%(message)s")
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 fh.setFormatter(formatter)
 ch.setFormatter(formatter)
-# add the handlers to the logger
+
 logger.addHandler(fh)
 logger.addHandler(ch)
 
